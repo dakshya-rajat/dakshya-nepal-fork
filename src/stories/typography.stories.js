0f2023471c1279ac4bbf447551cba3bb9d161a82
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-07 14:58:13 +0545

    Add stories to show the heading styles

---
+++ src/stories/typography.stories.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Heading from "../components/Heading"
+
+export default {
+  title: "Design/Typography",
+}
+
+export const HeadingStyles = () => {
+  return (
+    <>
+      <Heading code={1}>heading one</Heading>
+      <Heading code={2}>heading two</Heading>
+      <Heading code={3}>heading three</Heading>
+      <Heading code={4}>heading four</Heading>
+    </>
+  )
+}
