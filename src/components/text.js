7cd40a7723328fe090f6c812df7f4233b99b7eea
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-07 21:05:50 +0545

    Add all text styles to a component

--- src/components/text.js
+++ src/components/text.js
@@ -2,5 +2,22 @@ import React from "react"
 import { Text } from "grommet"
 
 export default props => {
-  return <Text {...props}>{props.children}</Text>
+  if (props.code === "body") props = { ...props, size: "large" }
+  else if (props.code === "quote")
+    props = { ...props, size: "large", weight: 500 }
+  else if (props.code === "sub-r") props = { ...props, size: "small" }
+  else if (props.code === "sub-m")
+    props = { ...props, size: "small", weight: 500 }
+  else if (props.code === "label")
+    props = { ...props, size: "medium", weight: 700 }
+  else if (props.code === "button")
+    props = { ...props, size: "medium", weight: 700 }
+  return (
+    <Text
+      {...props}
+      style={props.code === "button" ? { textTransform: "capitalize" } : null}
+    >
+      {props.children}
+    </Text>
+  )
 }
