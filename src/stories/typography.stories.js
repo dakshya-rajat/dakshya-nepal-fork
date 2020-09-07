b388eb7046863bfc133b4d3e84a9847a0b7d6c9f
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-07 15:41:30 +0545

    Add the text component to the typography storybook

--- src/stories/typography.stories.js
+++ src/stories/typography.stories.js
@@ -1,17 +1,26 @@
 import React from "react"
 import Heading from "../components/heading"
+import Text from "../components/text"
 
 export default {
   title: "Design/Typography",
 }
 
-export const HeadingStyles = () => {
+export const headingStyles = () => {
   return (
     <>
       <Heading code={1}>heading one</Heading>
       <Heading code={2}>heading two</Heading>
       <Heading code={3}>heading three</Heading>
       <Heading code={4}>heading four</Heading>
+    </>
+  )
+}
+
+export const textStyles = () => {
+  return (
+    <>
+      <Text color="brand">test test</Text>
     </>
   )
 }
