eda602e7613fe536e468da01dc9e37a1ebe81e5f
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-09 00:41:15 +0545

    Add another card variant with padding in the storybook

--- src/stories/cards.stories.js
+++ src/stories/cards.stories.js
@@ -7,8 +7,12 @@ export default {
 
 export const cardsWithPadding = () => {
   return (
-    <Card>
-      This is a card element. This has a default padding which is 16px.
-    </Card>
+    <>
+      <Card>
+        This is a card element. This has a default padding which is 16px.
+      </Card>
+      <p></p>
+      <Card p24>This is a card element. This has a padding of 24px.</Card>
+    </>
   )
 }
