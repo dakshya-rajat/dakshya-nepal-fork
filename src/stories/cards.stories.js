4cd40dd974d7e88db5ce60593da2aeda773cdd42
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-09 00:39:37 +0545

    Add a card with default styles to showcase

---
+++ src/stories/cards.stories.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "../components/card"
+
+export default {
+  title: "Design/Cards",
+}
+
+export const cardsWithPadding = () => {
+  return (
+    <Card>
+      This is a card element. This has a default padding which is 16px.
+    </Card>
+  )
+}
