6ed9d4200e2fe0d589cbc09f515d4f6c85e4a4f6
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-09 15:24:28 +0545

    Update stories to display newly added icons

--- src/stories/icons.stories.js
+++ src/stories/icons.stories.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Avatar, Box } from "grommet"
+import { Avatar, Box, TextInput, Button } from "grommet"
 import ArrowLeft from "../components/icons/arrow-left"
 import ArrowRight from "../components/icons/arrow-right"
 import Call from "../components/icons/call"
@@ -18,6 +18,12 @@ import Search from "../components/icons/search"
 import Twitter from "../components/icons/twitter"
 import User from "../components/icons/user"
 
+//colored icons
+import Book from "../images/icons/Book.svg"
+import Building from "../images/icons/Building.svg"
+import Domain from "../images/icons/Domain.svg"
+import Globe from "../images/icons/Globe.svg"
+
 export default {
   title: "Design/Icons",
 }
@@ -25,10 +31,7 @@ export default {
 export const controlIconList = () => {
   return (
     <div>
-      <p>
-        These are some of the control icons. These can be used in various places
-        like buttons, avatars, form controls, etc{" "}
-      </p>
+      <h4>These are some of the control icons.</h4>
       <Box direction="row" gap="small" wrap>
         <Box direction="column">
           <Avatar alignSelf="center">
@@ -133,6 +136,45 @@ export const controlIconList = () => {
           <pre>user</pre>
         </Box>
       </Box>
+      <Box direction="column">
+        <h4>
+          These can be used in various places like buttons, avatars, form
+          controls, etc
+        </h4>
+        <Box direction="row" gap="small">
+          <TextInput icon={<Search />} placeholder="Search" />
+          <TextInput icon={<User />} placeholder="Username" />
+          <TextInput icon={<Location />} placeholder="Address" />
+        </Box>
+        <Box direction="row" gap="small" margin={{ top: "medium" }}>
+          <Button
+            icon={<Menu />}
+            alignSelf="center"
+            label="Menu"
+            onClick={() => {}}
+            secondary
+          />
+          <Button
+            icon={<Exit />}
+            alignSelf="center"
+            onClick={() => {}}
+            secondary
+            hoverIndicator
+          />
+        </Box>
+      </Box>
     </div>
+  )
+}
+
+export const coloredIcons = () => {
+  return (
+    <Box direction="row" wrap gap="medium">
+      <Avatar round="none" src={Book} size="large" />
+      <Avatar round="none" src={Building} size="large" />
+      <Avatar round="none" src={Domain} size="large" />
+      <Avatar round="none" src={Globe} size="large" />
+      <h4>All the other svg illustrations can also be used like this</h4>
+    </Box>
   )
 }
