b4b38429349623fa4b3b059ab889285f5317a0bd
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-09 13:14:38 +0545

    Add remaining icons to the app

---
+++ src/components/icons/location.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Blank } from "grommet-icons"
+
+export default props => {
+  return (
+    <Blank {...props}>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M19.1998 10.3636C19.1998 16.0909 11.9998 21 11.9998 21C11.9998 21 4.7998 16.0909 4.7998 10.3636C4.7998 8.41068 5.55837 6.53771 6.90864 5.15676C8.2589 3.77581 10.0902 3 11.9998 3C13.9094 3 15.7407 3.77581 17.091 5.15676C18.4412 6.53771 19.1998 8.41068 19.1998 10.3636Z"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
+        <path
+          d="M11.9998 12.9C13.491 12.9 14.6998 11.6912 14.6998 10.2C14.6998 8.70883 13.491 7.5 11.9998 7.5C10.5086 7.5 9.2998 8.70883 9.2998 10.2C9.2998 11.6912 10.5086 12.9 11.9998 12.9Z"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
+      </svg>
+    </Blank>
+  )
+}
