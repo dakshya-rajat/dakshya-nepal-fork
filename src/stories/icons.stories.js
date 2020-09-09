7f6eecbfe6d20c935511f18f8c20cfff3ed00050
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-09 13:15:00 +0545

    Update the newly added icons in storybook

--- src/stories/icons.stories.js
+++ src/stories/icons.stories.js
@@ -4,6 +4,19 @@ import ArrowLeft from "../components/icons/arrow-left"
 import ArrowRight from "../components/icons/arrow-right"
 import Call from "../components/icons/call"
 import ChevronDown from "../components/icons/chevron-down"
+import ChevronLeft from "../components/icons/chevron-left"
+import ChevronRight from "../components/icons/chevron-right"
+import ChevronUp from "../components/icons/chevron-up"
+import Email from "../components/icons/email"
+import Exit from "../components/icons/exit"
+import Facebook from "../components/icons/facebook"
+import Instagram from "../components/icons/instagram"
+import Linkedin from "../components/icons/linkedin"
+import Location from "../components/icons/location"
+import Menu from "../components/icons/menu"
+import Search from "../components/icons/search"
+import Twitter from "../components/icons/twitter"
+import User from "../components/icons/user"
 
 export default {
   title: "Design/Icons",
@@ -16,7 +29,7 @@ export const controlIconList = () => {
         These are some of the control icons. These can be used in various places
         like buttons, avatars, form controls, etc{" "}
       </p>
-      <Box direction="row" gap="small">
+      <Box direction="row" gap="small" wrap>
         <Box direction="column">
           <Avatar alignSelf="center">
             <ArrowLeft />
@@ -40,6 +53,84 @@ export const controlIconList = () => {
             <ChevronDown />
           </Avatar>
           <pre>chevron-down</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <ChevronLeft />
+          </Avatar>
+          <pre>chevron-left</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <ChevronRight />
+          </Avatar>
+          <pre>chevron-right</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <ChevronUp />
+          </Avatar>
+          <pre>chevron-up</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Email />
+          </Avatar>
+          <pre>email</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Exit />
+          </Avatar>
+          <pre>exit</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Facebook />
+          </Avatar>
+          <pre>facebook</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Instagram />
+          </Avatar>
+          <pre>instagram</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Linkedin />
+          </Avatar>
+          <pre>linkedin</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Location />
+          </Avatar>
+          <pre>location</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Menu />
+          </Avatar>
+          <pre>menu</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Search />
+          </Avatar>
+          <pre>search</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <Twitter />
+          </Avatar>
+          <pre>twitter</pre>
+        </Box>
+        <Box direction="column">
+          <Avatar alignSelf="center">
+            <User />
+          </Avatar>
+          <pre>user</pre>
         </Box>
       </Box>
     </div>
