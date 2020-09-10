1d0778f63db1c829299870c22e8839155f844c70
Yogendra Rawl <yogendra.rawal@dakshyanepal.com>
2020-09-10 22:26:37 +0545

    Add primary button with disabled variant in storybook

--- src/components/button.js
+++ src/components/button.js
@@ -2,5 +2,5 @@ import React from "react"
 import { Button } from "grommet"
 
 export default props => {
-  return <Button {...props}>props.children</Button>
+  return <Button {...props}>{props.children}</Button>
 }
---
+++ src/stories/buttons.stories.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Button from "../components/button"
+import Text from "../components/text"
+import { Box } from "grommet"
+
+export default {
+  title: "Design/Controls",
+}
+
+export const buttons = () => {
+  return (
+    <Box direction="row" gap="medium">
+      <Button primary label={<Text code="button">Primary Button</Text>} />
+      <Button
+        primary
+        disabled
+        label={<Text code="button">Primary Button</Text>}
+      />
+    </Box>
+  )
+}
