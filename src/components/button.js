daafaee59d470772a318a5821b041919138a74be
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-16 20:16:40 +0545

    Add a check for label text style

--- src/components/button.js
+++ src/components/button.js
@@ -3,5 +3,14 @@ import { Button } from "grommet"
 import Text from "./text"
 
 export default props => {
-  return <Button {...props} label={<Text code="button">{props.label}</Text>} />
+  return (
+    <Button
+      {...props}
+      label={
+        <Text code={props.primary || props.secondary ? "button" : "subtext-m"}>
+          {props.label}
+        </Text>
+      }
+    />
+  )
 }
