913fb081b0d464e013bb1afc14b8af6bf306ed95
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-16 19:37:38 +0545

    Add default text style to the button

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Button } from "grommet"
+import Text from "./text"
 
 export default props => {
-  return <Button {...props}>{props.children}</Button>
+  return <Button {...props} label={<Text code="button">{props.label}</Text>} />
 }
