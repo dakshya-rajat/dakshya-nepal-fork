2c5ba8b349b2b502de61b6cda0d6af49029cbd28
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-17 10:36:33 +0545

    Add button labels to storybook

--- src/stories/buttons.stories.js
+++ src/stories/buttons.stories.js
@@ -11,10 +11,12 @@ export const buttons = () => {
   return (
     <Box direction="column" gap="medium">
       <Box direction="row" gap="medium">
+        <Text color="h1">Default Button</Text>
         <Button label="ghost" />
         <Button disabled label="ghost" />
       </Box>
       <Box direction="row" gap="medium">
+        <Text color="h1">Ka ("lang" was conflicting) Button</Text>
         <Button ka label="क" />
         <Button ka disabled label="क" />
       </Box>
