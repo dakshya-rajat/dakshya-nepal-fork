97d57261e1d5c0f0b55a02c00edf0515e8718827
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-18 21:32:54 +0545

    Add form field component extending grommet

---
+++ src/components/formField.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { FormField, TextInput } from "grommet"
+
+export default props => {
+  return (
+    <FormField {...props}>
+      <TextInput
+        placeholder={props.placeholder}
+        size="small"
+        icon={props.icon}
+      />
+    </FormField>
+  )
+}
