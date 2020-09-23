e6fdf3adeb61030063c78aa12dddf12e46c7f909
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-23 16:22:11 +0545

    Add the sample header to a story

---
+++ src/stories/layout.stories.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../sections/Layout/header"
+
+export default {
+  title: "Sections/Layout",
+}
+
+export const header = () => {
+  return <Header />
+}
