438cb44077debc3b4ee999cea8a881e37b6d3dd5
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-25 13:12:23 +0545

    Add css reset to global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,7 @@
 import React from "react"
 import { Grommet } from "grommet"
 import dakshyaTheme from "./grommet.theme"
+import "./src/styles/reset.css"
 
 // Wraps every page in a component
 export const wrapPageElement = ({ element, props }) => {
