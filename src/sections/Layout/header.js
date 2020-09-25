712d04a59d715c73327daaa18c65e522ea4db5a1
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-25 10:51:08 +0545

    Add responsive mobile button using layers

--- src/sections/Layout/header.js
+++ src/sections/Layout/header.js
@@ -1,53 +1,92 @@
 import React from "react"
-import { Header, Box, Menu, ResponsiveContext, Image } from "grommet"
-
+import { Header, Box, ResponsiveContext, Image, Layer, List } from "grommet"
 import Button from "../../components/button"
-
-import LogoHorizontal from "../../images/logo-horizontal.svg"
+import Text from "../../components/text"
 
 import MenuIcon from "../../components/icons/menu"
+import Exit from "../../components/icons/exit"
+import LogoHorizontal from "../../images/logo-horizontal.svg"
 
 export default props => {
+  const size = React.useContext(ResponsiveContext)
+  const [showLayer, setShowLayer] = React.useState(false)
+  const locations = ["Products", "Blogs", "About", "Careers", "Language: क"]
+
   return (
-    <Header
-      background="w1"
-      pad={{ horizontal: "xlarge", vertical: "medium" }}
-      elevation="header"
-    >
-      <a href="/">
-        <Image src={LogoHorizontal} label="Dakshya Nepal" />
-      </a>
-      <ResponsiveContext.Consumer>
-        {size =>
-          size === "small" ? (
-            <Box justify="end">
-              <Menu
-                a11yTitle="Navigation Menu"
-                dropProps={{ align: { top: "bottom", right: "right" } }}
-                icon={<MenuIcon />}
-                items={[
-                  {
-                    label: <Box pad="small">Mobile</Box>,
-                    href: "#",
-                  },
-                  {
-                    label: <Box pad="small">Menu</Box>,
-                    href: "#",
-                  },
-                ]}
-              />
-            </Box>
-          ) : (
-            <Box justify="end" direction="row" gap="medium">
-              <Button label="Products" />
-              <Button label="Blogs" />
-              <Button label="About" />
-              <Button label="Careers" />
-              <Button ka label="क" />
-            </Box>
-          )
+    <div>
+      <Header
+        background="w1"
+        pad={
+          size === "small" ? "16px" : { horizontal: "131px", vertical: "24px" }
         }
-      </ResponsiveContext.Consumer>
-    </Header>
+        elevation="header"
+      >
+        <a href="/">
+          <Image src={LogoHorizontal} label="Dakshya Nepal" />
+        </a>
+        <ResponsiveContext.Consumer>
+          {size =>
+            size === "small" ? (
+              <Box justify="end" onClick={() => setShowLayer(true)}>
+                <MenuIcon />
+              </Box>
+            ) : (
+              <Box justify="end" direction="row" gap="medium">
+                <Button label="Products" />
+                <Button label="Blogs" />
+                <Button label="About" />
+                <Button label="Careers" />
+                <Button ka label="क" />
+              </Box>
+            )
+          }
+        </ResponsiveContext.Consumer>
+      </Header>
+      {showLayer && (
+        <Layer full animation="fadeIn">
+          <Header
+            background="w1"
+            pad={
+              size === "small"
+                ? "16px"
+                : { horizontal: "131px", vertical: "24px" }
+            }
+            elevation="header"
+          >
+            <a href="/">
+              <Image src={LogoHorizontal} label="Dakshya Nepal" />
+            </a>
+            <Box justify="end" onClick={() => setShowLayer(false)}>
+              <Exit />
+            </Box>
+          </Header>
+          <Box
+            fill
+            background="transparent"
+            pad={{ horizontal: "large", vertical: "none" }}
+            margin={{ vertical: "small" }}
+          >
+            <List
+              data={locations}
+              border={{ color: "w2", side: "bottom" }}
+              fill
+              pad={{ vertical: "small", horizontal: "none" }}
+            >
+              {(datum, index) => (
+                <a
+                  href="#"
+                  style={{ color: "inherit", textDecoration: "none" }}
+                  key={index}
+                >
+                  <Text code="body" style={{ lineHeight: "32px" }}>
+                    {datum}
+                  </Text>
+                </a>
+              )}
+            </List>
+          </Box>
+        </Layer>
+      )}
+    </div>
   )
 }
