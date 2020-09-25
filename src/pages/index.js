18b45195ffc0ece9e148eff65b9ee53c775ed4f5
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-25 13:13:44 +0545

    Use layout in the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import Seo from "../components/seo"
 import { Heading, Box } from "grommet"
+import Layout from "../sections/Layout"
 
 export default () => {
   return (
-    <>
+    <Layout>
       <Seo title="Home" />
       <Box minHeight="100%" width="100%">
-        <Heading level={2}>test test</Heading>
+        <Heading level={2}>Home Page</Heading>
       </Box>
-    </>
+    </Layout>
   )
 }
