0193fa5f195fd3bfefe862fd64ce50a9eb5c0263
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-29 18:00:07 +0545

    Add slider to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
 import Seo from "../components/seo"
-import { Heading, Box } from "grommet"
 import Layout from "../sections/layout"
+import Slider from "../sections/slider"
 
 export default () => {
   return (
     <Layout>
       <Seo title="Home" />
-      <Box minHeight="100%" width="100%">
-        <Heading level={2}>Home Page</Heading>
-      </Box>
+      <Slider />
     </Layout>
   )
 }
