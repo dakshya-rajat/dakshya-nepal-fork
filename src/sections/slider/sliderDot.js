4b2536f70a1f253324ac79669ad7ff1c55566474
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-09-29 17:58:38 +0545

    Update slider dots for our slider

---
+++ src/sections/slider/sliderDot.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Box } from "grommet"
+
+export default () => {
+  return (
+    <a>
+      <Box width="100%" height="8px" background="g1" round />
+    </a>
+  )
+}
