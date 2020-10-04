63b0a6969e6bba66908caf99ccd0d378c3af2918
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-04 15:15:20 +0545

    Add responsive check

--- src/sections/onlineClasses/index.js
+++ src/sections/onlineClasses/index.js
@@ -1,15 +1,25 @@
 import React from "react"
-import { Box, Image } from "grommet"
+import { Box, ResponsiveContext } from "grommet"
 import Heading from "../../components/heading"
 import Button from "../../components/button"
 import classes from "./classes.json"
 import OnlineClassBlock from "./onlineClassBlock"
 import NewTutor from "./newTutor"
+import ComponentSlider from "../componentSlider"
 
 export default props => {
+  const mobile = React.useContext(ResponsiveContext) === "small"
+
   return (
-    <Box pad={{ horizontal: "131.5px", top: "98px" }}>
-      <Box direction="row">
+    <Box>
+      <Box
+        direction="row"
+        pad={
+          mobile
+            ? { horizontal: "16px", top: "64px" }
+            : { horizontal: "131.5px", top: "98px" }
+        }
+      >
         <Heading code={2} size="medium">
           online classes
         </Heading>
