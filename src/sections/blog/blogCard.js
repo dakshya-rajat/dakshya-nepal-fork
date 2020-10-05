551c511e9a00abccdc905c9fba95e94fcbedc9f9
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-05 11:55:20 +0545

    Add dynamic data to the blog card component

--- src/sections/blog/blogCard.js
+++ src/sections/blog/blogCard.js
@@ -3,23 +3,23 @@ import { Image, Box } from "grommet"
 import Text from "../../components/text"
 import Heading from "../../components/heading"
 
-export default ({ mobile }) => {
+export default ({ mobile, title, image, minRead, category }) => {
   return (
     <Box width={mobile ? "98%" : "33.33%"} round="4px" border={{ color: "w2" }}>
-      <Image src="https://source.unsplash.com/random/352x200" />
+      <Image src={image} />
       <Box pad={{ horizontal: "16px", top: "16px", bottom: "40px" }}>
         <Box direction="row">
           <Text code="subtext-r" color="g1" size="small">
-            6 min read
+            {minRead} min read
           </Text>
           <Box flex="grow">
             <Text code="subtext-r" color="g1" size="small" alignSelf="end">
-              #Nepal
+              #{category}
             </Text>
           </Box>
         </Box>
         <Heading code={4} margin={{ top: "8px" }}>
-          Education as a burden to overcome.
+          {title}
         </Heading>
       </Box>
     </Box>
