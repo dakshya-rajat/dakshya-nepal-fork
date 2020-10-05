1a7a92fbaec0c4b5953036498164b0450385b0e7
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-05 11:01:52 +0545

    Add blog card

---
+++ src/sections/blog/blogCard.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Box, Image } from "grommet"
+import Text from "../../components/text"
+import Heading from "../../components/heading"
+
+export default props => {
+  return (
+    <Box width="33.33%" round="4px" border={{ color: "w2" }}>
+      <Image src="https://source.unsplash.com/random/352x200" />
+      <Box pad={{ horizontal: "16px", top: "16px", bottom: "40px" }}>
+        <Box direction="row">
+          <Text code="subtext-r" color="g1" size="small">
+            6 min read
+          </Text>
+          <Box flex="grow">
+            <Text code="subtext-r" color="g1" size="small" alignSelf="end">
+              #Nepal
+            </Text>
+          </Box>
+        </Box>
+        <Heading code={4} margin={{ top: "8px" }}>
+          Education as a burden to overcome.
+        </Heading>
+      </Box>
+    </Box>
+  )
+}
