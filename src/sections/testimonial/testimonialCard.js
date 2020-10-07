bcaee653be48e89586063bd415b48d2be222f05e
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-07 14:25:00 +0545

    Add testimonial card for data

---
+++ src/sections/testimonial/testimonialCard.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Text from "../../components/text"
+import { Avatar, Box } from "grommet"
+import Heading from "../../components/heading"
+import Card from "../../components/card"
+
+export default ({ name, occupation, message, image }) => {
+  return (
+    <Card pad="24px" border={{ color: "w2" }} width="33.33%" gap="medium">
+      <Text code="subtext-r" size="small">
+        {message}
+      </Text>
+      <Box direction="row" gap="small">
+        <Avatar round src={image} />
+        <Box>
+          <Heading code={4}>{name}</Heading>
+          <Text code="subtext-m" size="small" color="b2">
+            {occupation}
+          </Text>
+        </Box>
+      </Box>
+    </Card>
+  )
+}
