9f6e81c14a44c335660ae3025c0d5d3133b629da
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-07 14:25:35 +0545

    Add a section for displaying testimonial

---
+++ src/sections/testimonial/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Box } from "grommet"
+import Heading from "../../components/heading"
+import ComponentSlider from "../componentSlider"
+import TestimonialCard from "./testimonialCard"
+import tesimonialData from "./testimonialData.json"
+
+export default props => {
+  return (
+    <Box>
+      <Box pad={{ top: "90px", horizontal: "131.5px" }}>
+        <Heading code={2}>Our Happy Costumers</Heading>
+      </Box>
+      <ComponentSlider margin={{ top: "48px" }}>
+        {tesimonialData.map((data, index) => (
+          <TestimonialCard
+            name={data.name}
+            occupation={data.occupation}
+            message={data.message}
+            image={data.image}
+            key={index}
+          />
+        ))}
+      </ComponentSlider>
+    </Box>
+  )
+}
