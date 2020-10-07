520efc1b33ce09af6abbc4b5cfc3602190f292e5
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-08 03:49:47 +0545

    Add product list section to the product page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Layout from "../sections/layout"
 import SEO from "../components/seo"
+import ProductList from "../sections/productList"
 
 export default () => {
   return (
     <Layout active="products">
       <SEO title="Products" />
-      <h1>Products page</h1>
+      <ProductList />
     </Layout>
   )
 }
