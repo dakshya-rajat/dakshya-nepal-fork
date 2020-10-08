7ae7674f7111d56e0b230dba27ad311c95db9603
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-08 11:27:42 +0545

    Add cta section to the product page

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../sections/layout"
 import SEO from "../components/seo"
 import ProductList from "../sections/productList"
+import CtaBox from "../sections/ctaBox"
 
 export default () => {
   return (
     <Layout active="products">
       <SEO title="Products" />
       <ProductList />
+      <CtaBox marginTop marginBottom />
     </Layout>
   )
 }
