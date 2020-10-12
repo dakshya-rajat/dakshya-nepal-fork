3cd003ec76bad26f293936b7ddd94539d4756d81
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-12 10:54:57 +0545

    Update responsive design for the text slider in about page

--- src/sections/verticalText/index.js
+++ src/sections/verticalText/index.js
@@ -1,35 +1,44 @@
 import React from "react"
-import { Box, Image } from "grommet"
+import { Box, Image, ResponsiveContext } from "grommet"
 import textList from "./textList"
 import TextSlider from "./textSlider"
 
 export default props => {
+  const mobile = React.useContext(ResponsiveContext) === "small"
+
   return (
-    <Box pad={{ top: "31px", horizontal: "131px" }}>
-      <Box direction="row" gap="medium">
-        <Box flex="shrink" height="medium">
+    <Box
+      pad={
+        mobile
+          ? { top: "49px", horizontal: "16px" }
+          : { top: "31px", horizontal: "131px" }
+      }
+    >
+      <Box direction={mobile ? "column" : "row"} gap="medium">
+        <Box height="medium" width={mobile ? "100%" : "50%"}>
           <TextSlider textList={textList} />
           <Box
-            height="62%"
-            width="30%"
-            background="linear-gradient(180deg,#FFFBEF 13%,rgba(254,250,237,0) 87%)"
+            height={mobile ? "200%" : "62%"}
+            width={mobile ? "100%" : "30%"}
+            background={`linear-gradient(180deg,#FFFBEF 13%,rgba(254,250,237,0) 87%)`}
             style={{
               position: "absolute",
               transform: "rotate(-180deg)",
               pointerEvents: "none",
+              left: mobile ? "0" : null,
             }}
           />
           <Box
-            height="12%"
-            width="30%"
-            background="linear-gradient(180deg, #FFFFFE 0%, rgba(254, 250, 237, 0) 87.5%)"
+            height={mobile ? "30%" : "12%"}
+            width={mobile ? "88%" : "30%"}
+            background={`linear-gradient(180deg, #FFFFFE 0%, rgba(254, 250, 237, 0) 87.5%)`}
             style={{
               position: "absolute",
               pointerEvents: "none",
             }}
           />
         </Box>
-        <Box flex="grow">
+        <Box width={mobile ? "100%" : "50%"}>
           <Image src="illustrations/about us.svg" />
         </Box>
       </Box>
