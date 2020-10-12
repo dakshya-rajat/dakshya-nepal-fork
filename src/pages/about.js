fdb0bcb216a0c368ac98a709e4d716ac85d7d2eb
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-12 11:56:03 +0545

    Add feature section for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../sections/layout"
 import SEO from "../components/seo"
 import VerticalText from "../sections/verticalText"
+import FeatureAbout from "../sections/featureAbout"
 
 export default () => {
   return (
     <Layout active="about">
       <SEO title="About" />
       <VerticalText />
+      <FeatureAbout />
     </Layout>
   )
 }
