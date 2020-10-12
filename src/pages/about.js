5e9d00d96a8e33d9fd5aae8b3c2309a709acd3b1
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-12 14:23:51 +0545

    Add benefit section to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from "../sections/layout"
 import SEO from "../components/seo"
 import VerticalText from "../sections/verticalText"
 import FeatureAbout from "../sections/featureAbout"
+import BenefitSection from "../sections/benefitSection"
 
 export default () => {
   return (
@@ -10,6 +11,7 @@ export default () => {
       <SEO title="About" />
       <VerticalText />
       <FeatureAbout />
+      <BenefitSection />
     </Layout>
   )
 }
