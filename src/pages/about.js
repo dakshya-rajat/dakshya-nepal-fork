f8556be9a784fc3376b16243ed5583d67695e01c
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-12 15:40:05 +0545

    Add cta section to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import VerticalText from "../sections/verticalText"
 import FeatureAbout from "../sections/featureAbout"
 import BenefitSection from "../sections/benefitSection"
 import Team from "../sections/team"
+import CtaBox from "../sections/ctaBox"
 
 export default () => {
   return (
@@ -14,6 +15,7 @@ export default () => {
       <FeatureAbout />
       <BenefitSection />
       <Team />
+      <CtaBox marginTop marginBottom />
     </Layout>
   )
 }
