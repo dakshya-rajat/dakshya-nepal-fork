7737c13deb291a58dcc4386551516001816f0b65
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-13 13:14:37 +0545

    Add careers page to the website

---
+++ src/pages/careers.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../sections/layout"
+import Seo from "../components/seo"
+
+export default props => {
+  return (
+    <Layout>
+      <Seo title="Careers" />
+    </Layout>
+  )
+}
