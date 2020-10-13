fc06bad6989c2651d25ed3a2c64f5ac881606f73
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-13 13:16:49 +0545

    Add join us section to careers page

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../sections/layout"
 import Seo from "../components/seo"
+import JoinUs from "../sections/joinUs"
 
 export default props => {
   return (
     <Layout>
       <Seo title="Careers" />
+      <JoinUs />
     </Layout>
   )
 }
