18df2b091550698fe95672fa85a5e574b8521982
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-13 13:56:00 +0545

    Add responsive design for the joinus section

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../sections/layout"
 import Seo from "../components/seo"
 import JoinUs from "../sections/joinUs"
+import CareerDropdown from "../sections/careersDropdown"
 
 export default props => {
   return (
     <Layout>
       <Seo title="Careers" />
       <JoinUs />
+      <CareerDropdown />
     </Layout>
   )
 }
--- src/sections/joinUs/index.js
+++ src/sections/joinUs/index.js
@@ -1,16 +1,28 @@
 import React from "react"
-import { Box, Grid } from "grommet"
+import { Box, Grid, ResponsiveContext } from "grommet"
 import Heading from "../../components/heading"
 import JoinUs from "./joinus"
 import joinus from "./joinUsData.json"
 
 export default props => {
+  const mobile = React.useContext(ResponsiveContext) === "small"
+
   return (
-    <Box pad={{ top: "34px", horizontal: "131px" }}>
+    <Box
+      pad={
+        mobile
+          ? { top: "28px", horizontal: "16px" }
+          : { top: "34px", horizontal: "131px" }
+      }
+    >
       <Heading code={2}>Careers</Heading>
-      <Grid pad={{ top: "24px" }} columns="45%" gap="medium">
+      <Grid
+        pad={{ top: "24px" }}
+        columns={mobile ? "100%" : "45%"}
+        gap="medium"
+      >
         {joinus.map((data, index) => (
-          <JoinUs data={data} key={index} />
+          <JoinUs data={data} key={index} mobile={mobile} />
         ))}
       </Grid>
     </Box>
