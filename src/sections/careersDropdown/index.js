b8cddb9a976877fdae98813478878c6783bceeaf
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-13 16:31:43 +0545

    Add responsive design to dropdown section of career page

--- src/sections/careersDropdown/index.js
+++ src/sections/careersDropdown/index.js
@@ -1,12 +1,20 @@
 import React from "react"
-import { Box } from "grommet"
+import { Box, ResponsiveContext } from "grommet"
 import Heading from "../../components/heading"
 import Dropdown from "./dropdown"
 import careers from "./careers.json"
 
 export default props => {
+  const mobile = React.useContext(ResponsiveContext) === "small"
+
   return (
-    <Box pad={{ top: "90px", horizontal: "131px" }}>
+    <Box
+      pad={
+        mobile
+          ? { top: "90px", horizontal: "16px" }
+          : { top: "58px", horizontal: "131px" }
+      }
+    >
       <Heading code={2}>open Careers @ dakshya nepal</Heading>
       <Box pad={{ top: "40px" }} gap="medium">
         {careers.map((data, index) => (
