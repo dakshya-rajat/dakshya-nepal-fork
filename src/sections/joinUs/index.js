a6fe3526db8808954697d7f277d850f7e891df2d
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-13 13:47:07 +0545

    Update join us section to use the block

--- src/sections/joinUs/index.js
+++ src/sections/joinUs/index.js
@@ -1,6 +1,18 @@
 import React from "react"
-import { Box } from "grommet"
+import { Box, Grid } from "grommet"
+import Heading from "../../components/heading"
+import JoinUs from "./joinus"
+import joinus from "./joinUsData.json"
 
 export default props => {
-  return <Box>test test</Box>
+  return (
+    <Box pad={{ top: "34px", horizontal: "131px" }}>
+      <Heading code={2}>Careers</Heading>
+      <Grid pad={{ top: "24px" }} columns="45%" gap="medium">
+        {joinus.map((data, index) => (
+          <JoinUs data={data} key={index} />
+        ))}
+      </Grid>
+    </Box>
+  )
 }
