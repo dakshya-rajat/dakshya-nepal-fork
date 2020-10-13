82c66571d6351056530bcbb214d5b4d5d613b573
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-13 15:25:30 +0545

    Add background position for join us image

--- src/sections/joinUs/joinus.js
+++ src/sections/joinUs/joinus.js
@@ -4,18 +4,29 @@ import Heading from "../../components/heading"
 import Text from "../../components/text"
 import Button from "../../components/button"
 
-export default ({ data }) => {
+export default ({ data, mobile }) => {
   return (
-    <Box direction="row" border={{ size: "2px", color: "w2" }} round="4px">
+    <Box
+      direction={mobile ? "column" : "row"}
+      border={{ size: "2px", color: "w2" }}
+      round="4px"
+    >
       <Box
         background="p1-l"
         overflow="hidden"
         width="large"
         pad={{ top: "medium" }}
       >
-        <Image src={data.image} alignSelf={data.background} />
+        <Image
+          src={data.image}
+          alignSelf={mobile ? "center" : data.background}
+        />
       </Box>
-      <Box gap="xsmall" pad={{ horizontal: "24px" }} alignSelf="center">
+      <Box
+        gap="small"
+        pad={{ horizontal: "24px", vertical: "24px" }}
+        alignSelf="center"
+      >
         <Heading code={3}>{data.title}</Heading>
         <Text code="subtext-r" size="small" color="b2">
           {data.desc}
