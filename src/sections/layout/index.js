901ca6cb4d3f392973ce5f08cc7318c22ee501d3
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-14 12:03:40 +0545

    Add animation check for animating the homepage only

--- src/sections/layout/index.js
+++ src/sections/layout/index.js
@@ -1,13 +1,21 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+import { Box } from "grommet"
 
-export default props => {
+const Layout = ({ children, active }) => {
+  const location = typeof window !== "undefined" ? window.location.pathname : ""
   return (
     <div>
-      <Header active={props.active} />
-      {props.children}
+      <Box animation={location === "/" ? { type: "slideDown" } : "null"}>
+        <Header active={active} />
+      </Box>
+      <Box animation={location === "/" ? { type: "slideUp" } : ""}>
+        {children}
+      </Box>
       <Footer />
     </div>
   )
 }
+
+export default Layout
