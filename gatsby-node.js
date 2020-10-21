b93ed0f5a9961982873df67d90eada045b1f5de1
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-22 04:40:16 +0545

    Add a gatsby config for creating pages from the url of google doc

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+const _path = require(`path`)
+
+exports.createPages = async ({ graphql, actions }) =>
+  graphql(
+    `
+      {
+        allGoogleDocs {
+          nodes {
+            path
+          }
+        }
+      }
+    `
+  ).then(result => {
+    result.data.allGoogleDocs.nodes.forEach(({ path }, index) => {
+      actions.createPage({
+        path: path,
+        component: _path.resolve(`./src/templates/blog.js`),
+      })
+    })
+  })
