8fad1f1d35fe053826ee9a0f5eab7f8775b6c7dd
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-22 10:04:23 +0545

    Add blog page for displaying blog content

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../sections/layout"
+import SEO from "../components/seo"
+
+export default props => {
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      test test
+    </Layout>
+  )
+}
