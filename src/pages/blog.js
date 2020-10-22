a59d8b644c21af79b19aa4d6f9a72efbeeeec0a7
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-22 17:52:12 +0545

    Add query in the blog page for all blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,38 @@ import React from "react"
 
 import Layout from "../sections/layout"
 import SEO from "../components/seo"
+import Blog from "../sections/blog"
+import { graphql } from "gatsby"
 
-export default props => {
+export default ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <SEO title="Blog" />
-      test test
+      <Blog title="All Blogs" data={data} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allGoogleDocs {
+      nodes {
+        name
+        path
+        childMarkdownRemark {
+          timeToRead
+        }
+        cover {
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
