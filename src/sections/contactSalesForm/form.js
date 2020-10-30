04923ffc0a329327cbc1afd5ec8484154c3e4c30
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-30 10:02:28 +0545

    Update contact form to formik powered form

--- src/sections/contactSalesForm/form.js
+++ src/sections/contactSalesForm/form.js
@@ -2,47 +2,104 @@ import React from "react"
 import { Box, ResponsiveContext, TextArea } from "grommet"
 import FormField from "../../components/formField"
 import Button from "../../components/button"
+import { Formik } from "formik"
 
 export default ({ product }) => {
   const mobile = React.useContext(ResponsiveContext) === "small"
   return (
     <Box margin={{ vertical: "24px", horizontal: "16px" }} gap="xsmall">
-      <form>
-        <FormField
-          label="Name of Organization *"
-          placeholder="Input your organization name"
-        />
-        <FormField label="Address" placeholder="Input your Address" />
-        <FormField label="Your Email" placeholder="Input your email" />
-        <FormField
-          label="Contact Number *"
-          placeholder="Input your phone number"
-        />
-        <label
-          style={{
-            marginTop: "8px",
-            marginBottom: "8px",
-            fontSize: "14px",
-            lineHeight: "normal",
-          }}
-          htmlFor="message"
-        >
-          Messages (If any)
-        </label>
-        <TextArea
-          placeholder="Input your message"
-          size="small"
-          name="message"
-          style={{ border: "2px solid #EAEAEA" }}
-        />
-        <input type="hidden" value={product} />
-        <Button
-          primary
-          label="Contact Sales"
-          alignSelf={mobile ? null : "start"}
-          type="submit"
-        />
-      </form>
+      <Formik
+        initialValues={{
+          orgName: "",
+          address: "",
+          email: "",
+          phone: "",
+          message: "",
+          product: product,
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <FormField
+              label="Name of Organization *"
+              placeholder="Input your organization name"
+              name="orgName"
+              type="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              values={values.orgName}
+              required="true"
+            />
+            <FormField
+              label="Address"
+              placeholder="Input your Address"
+              name="address"
+              type="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              values={values.address}
+            />
+            <FormField
+              label="Your Email"
+              placeholder="Input your email"
+              name="email"
+              type="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              values={values.email}
+            />
+            <FormField
+              label="Contact Number *"
+              placeholder="Input your phone number"
+              name="phone"
+              type="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              values={values.number}
+              required="true"
+            />
+            <label
+              style={{
+                fontSize: "14px",
+                lineHeight: "normal",
+              }}
+              htmlFor="message"
+            >
+              Messages (If any)
+            </label>
+            <TextArea
+              placeholder="Input your message"
+              size="small"
+              name="message"
+              style={{ border: "2px solid #EAEAEA" }}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              values={values.message}
+            />
+            <input type="hidden" value={values.product} />
+            <Button
+              primary
+              label="Contact Sales"
+              alignSelf={mobile ? null : "start"}
+              type="submit"
+              disabled={isSubmitting}
+            />
+          </form>
+        )}
+      </Formik>
     </Box>
   )
 }
