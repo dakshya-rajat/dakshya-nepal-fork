0af41d7b5117ed4321195805eda3227eabb90867
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-10-30 09:29:58 +0545

    Add text area field for messages in the form

--- src/sections/contactSalesForm/form.js
+++ src/sections/contactSalesForm/form.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, ResponsiveContext } from "grommet"
+import { Box, ResponsiveContext, TextArea } from "grommet"
 import FormField from "../../components/formField"
 import Button from "../../components/button"
 
@@ -7,25 +7,42 @@ export default ({ product }) => {
   const mobile = React.useContext(ResponsiveContext) === "small"
   return (
     <Box margin={{ vertical: "24px", horizontal: "16px" }} gap="xsmall">
-      <FormField
-        label="Name of Organization *"
-        required="true"
-        placeholder="Input your organization name"
-      />
-      <FormField label="Address" placeholder="Input your Address" />
-      <FormField label="Your Email" placeholder="Input your email" />
-      <FormField
-        label="Contact Number *"
-        required="true"
-        placeholder="Input your phone number"
-      />
-      <FormField label="Messages (If any)" placeholder="Input your message" />
-      <input type="hidden" value={product} />
-      <Button
-        primary
-        label="Contact Sales"
-        alignSelf={mobile ? null : "start"}
-      />
+      <form>
+        <FormField
+          label="Name of Organization *"
+          placeholder="Input your organization name"
+        />
+        <FormField label="Address" placeholder="Input your Address" />
+        <FormField label="Your Email" placeholder="Input your email" />
+        <FormField
+          label="Contact Number *"
+          placeholder="Input your phone number"
+        />
+        <label
+          style={{
+            marginTop: "8px",
+            marginBottom: "8px",
+            fontSize: "14px",
+            lineHeight: "normal",
+          }}
+          htmlFor="message"
+        >
+          Messages (If any)
+        </label>
+        <TextArea
+          placeholder="Input your message"
+          size="small"
+          name="message"
+          style={{ border: "2px solid #EAEAEA" }}
+        />
+        <input type="hidden" value={product} />
+        <Button
+          primary
+          label="Contact Sales"
+          alignSelf={mobile ? null : "start"}
+          type="submit"
+        />
+      </form>
     </Box>
   )
 }
