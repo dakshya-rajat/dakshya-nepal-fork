f311b311f9939140d74005785a00e6be62c4f688
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-01 17:32:45 +0545

    Convert the guidance counseling form to formik enabled form

--- src/sections/guidanceCounseling/contactForm.js
+++ src/sections/guidanceCounseling/contactForm.js
@@ -5,6 +5,7 @@ import FormSideImage from "../../../static/illustrations/Guidance Counseling.svg
 import FormField from "../../components/formField"
 import Button from "../../components/button"
 import Text from "../../components/text"
+import { Formik, Field } from "formik"
 
 export default props => {
   const mobile = React.useContext(ResponsiveContext) === "small"
@@ -33,68 +34,145 @@ export default props => {
           </Heading>
         </Box>
       </Box>
-      <Box direction="row" border={{ color: "w2", size: "2px" }} round="8px">
-        <Box width={mobile ? "100%" : "50%"} pad="16px" gap="medium">
-          <Box gap="small">
-            <Box gap="xsmall">
-              <Text size="small">Need guidance as</Text>
-              <Select
-                placeholder="Select one"
-                size="small"
-                options={["Student", "Parent"]}
-                value={value}
-                onChange={({ option }) => setValue(option)}
-              />
+      <Formik
+        initialValues={{
+          name: "",
+          address: "",
+          phone: "",
+          school: "",
+          problems: "",
+          type: "",
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              direction="row"
+              border={{ color: "w2", size: "2px" }}
+              round="8px"
+            >
+              <Box width={mobile ? "100%" : "50%"} pad="16px" gap="medium">
+                <Box gap="small">
+                  <Box gap="xsmall">
+                    <Text size="small">Need guidance as</Text>
+                    <Select
+                      placeholder="Select one"
+                      size="small"
+                      options={["Student", "Parent"]}
+                      value={value}
+                      onChange={({ option }) => handleChange(option)}
+                    />
+                  </Box>
+                  <FormField
+                    label="Your Name *"
+                    placeholder="Input your name"
+                    name="name"
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.name}
+                    required="true"
+                  />
+                  <FormField
+                    label="Address"
+                    placeholder="Your Address"
+                    name="address"
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.address}
+                  />
+                  <FormField
+                    label="Contact Number *"
+                    name="phone"
+                    type="text"
+                    placeholder="Input your phone number"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.phone}
+                    required="true"
+                  />
+                  <FormField
+                    label="School Name"
+                    placeholder="Your school name as applicable"
+                    name="school"
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.school}
+                  />
+                  <Box gap="xsmall">
+                    <Text size="small">
+                      Nature of Counseling you are seeking
+                    </Text>
+                    <Select
+                      placeholder="Select one"
+                      size="small"
+                      options={[
+                        "Academics",
+                        "Relationship",
+                        "Social Life, others",
+                      ]}
+                      value={value}
+                      onChange={({ option }) => setValue(option)}
+                    />
+                  </Box>
+                  <FormField
+                    label="Describe the hardships you are facing in few words"
+                    placeholder="Your hardships"
+                    name="problems"
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.problems}
+                  />
+                  <Box gap="xsmall">
+                    <Text size="small">Available for Counseling via</Text>
+                    <Select
+                      placeholder="Select one"
+                      size="small"
+                      options={["Phone Call", "Video Call"]}
+                      value={value}
+                      onChange={({ option }) => setValue(option)}
+                    />
+                  </Box>
+                </Box>
+                <Box width={mobile ? "100%" : "small"}>
+                  <Button
+                    primary
+                    label="Book Your Session"
+                    disabled={isSubmitting}
+                    type="submit"
+                  />
+                </Box>
+              </Box>
+              <Box
+                width={mobile ? "0%" : "50%"}
+                background="p1-l"
+                alignContent="end"
+                justify="end"
+              >
+                <Image src={FormSideImage} fill="horizontal" />
+              </Box>
             </Box>
-            <FormField label="Your Name" placeholder="Input your name" />
-            <FormField label="Address" placeholder="Your Address" />
-            <FormField
-              label="Contact Number *"
-              required="true"
-              placeholder="Input your phone number"
-            />
-            <FormField
-              label="School Name"
-              placeholder="Your school name as applicable"
-            />
-            <Box gap="xsmall">
-              <Text size="small">Nature of Counseling you are seeking</Text>
-              <Select
-                placeholder="Select one"
-                size="small"
-                options={["Academics", "Relationship", "Social Life, others"]}
-                value={value}
-                onChange={({ option }) => setValue(option)}
-              />
-            </Box>
-            <FormField
-              label="Describe the hardships you are facing in few words"
-              placeholder="Your hardships"
-            />
-            <Box gap="xsmall">
-              <Text size="small">Available for Counseling via</Text>
-              <Select
-                placeholder="Select one"
-                size="small"
-                options={["Phone Call", "Video Call"]}
-                value={value}
-                onChange={({ option }) => setValue(option)}
-              />
-            </Box>
-          </Box>
-          <Box width={mobile ? "100%" : "small"}>
-            <Button primary label="Book Your Session" />
-          </Box>
-        </Box>
-        <Box
-          width={mobile ? "0%" : "50%"}
-          background="p1-l"
-          alignContent="end"
-          justify="end"
-        >
-          <Image src={FormSideImage} fill="horizontal" />
-        </Box>
-      </Box>
+          </form>
+        )}
+      </Formik>
     </Box>
   )
 }
