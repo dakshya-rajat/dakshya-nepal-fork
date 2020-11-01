ff9724aaca0c05abdb036f74cbac94ec08a204a5
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-01 16:50:49 +0545

    Update notification component to simplify the use

--- src/sections/notification/index.js
+++ src/sections/notification/index.js
@@ -5,16 +5,16 @@ import Text from "../../components/text"
 import Exit from "../../components/icons/exit"
 import { Validate, Alert } from "grommet-icons"
 
-export default ({ title, type, message }) => {
+export default ({ error, message }) => {
   const [open, setOpen] = React.useState(true)
-  const Icon = type === "success" ? Validate : Alert
+  const Icon = error ? Alert : Validate
 
   return (
     <Box style={{ position: "fixed", bottom: 0 }} width="80%">
       <Collapsible open={open}>
         <Box
           round={{ size: "8px", corner: "top" }}
-          background={type === "success" ? "s1" : "s2"}
+          background={error ? "s2" : "s1"}
           pad={{ vertical: "medium", horizontal: "large" }}
           align="center"
           justify="center"
@@ -24,7 +24,7 @@ export default ({ title, type, message }) => {
             <Box direction="row" gap="medium">
               <Icon size="large" />
               <Box alignSelf="center">
-                <Heading code={3}>{title}</Heading>
+                <Heading code={3}>{error ? "Error" : "Success"}</Heading>
                 <Text code="sub-m">{message}</Text>
               </Box>
             </Box>
