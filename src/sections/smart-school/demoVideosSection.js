cde0bc00ff8e3ee71ca7a08fd5919c70c4dccfce
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-03 17:40:10 +0545

    Update the video section to responsive screen

--- src/sections/smart-school/demoVideosSection.js
+++ src/sections/smart-school/demoVideosSection.js
@@ -4,8 +4,46 @@ import Heading from "../../components/heading"
 import Text from "../../components/text"
 import Button from "../../components/button"
 import Video from "./video"
+import videoData from "./videoData.json"
+import Slider from "react-slick"
 
-export default props => {
+const MobileSwitcher = props => {
+  const settings = {
+    dots: false,
+    infinite: false,
+    centerMode: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerPadding: "16px",
+  }
+
+  return (
+    <ResponsiveContext.Consumer>
+      {size =>
+        size === "small" ? (
+          <Slider {...props} {...settings}>
+            {props.children}
+          </Slider>
+        ) : (
+          <Grid direction="row" columns="30%">
+            {props.children}
+          </Grid>
+        )
+      }
+    </ResponsiveContext.Consumer>
+  )
+}
+
+export default () => {
+  const [activeClass, setActiveClass] = React.useState(videoData[0])
+  const [active, setActive] = React.useState(0)
+
+  const handleSwap = index => {
+    setActiveClass(videoData[index])
+    setActive(index)
+  }
+
   const mobile = React.useContext(ResponsiveContext) === "small"
   return (
     <Box
@@ -20,42 +58,47 @@ export default props => {
       <Box
         gap="medium"
         margin={{ top: "32px" }}
-        border={{ color: "w2", size: "2px" }}
-        direction="row"
-        height="536px"
+        border={mobile ? null : { color: "w2", size: "2px" }}
+        direction={mobile ? "column" : "row"}
+        height={mobile ? null : "536px"}
       >
         <Box
-          width="15%"
+          width={mobile ? "100%" : "15%"}
           pad={{ left: "24px", top: "24px" }}
           overflow="auto"
           className="noScrollBar"
           gap="small"
+          direction={mobile ? "row" : "column"}
         >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(data => (
-            <Button
-              style={{ borderRadius: "8px", paddingRight: "35px" }}
-              ghost
-              alignSelf="start"
-              label={`Class ${data}`}
-              key={data}
-            />
+          {videoData.map((data, index) => (
+            <Box flex="grow">
+              <Button
+                style={{ borderRadius: "8px", paddingRight: "35px" }}
+                ghost
+                alignSelf="start"
+                label={`Class ${index + 1}`}
+                key={index + 20}
+                onClick={() => handleSwap(index)}
+                active={active === index}
+              />
+            </Box>
           ))}
         </Box>
         <Box flex="grow" pad={{ top: "24px", right: "24px" }}>
-          <Grid direction="row" columns="30%" gap="medium">
-            {[1, 2, 3, 4, 5].map(data => (
-              <Box key={data} gap="small">
+          <MobileSwitcher>
+            {activeClass.map((data, index) => (
+              <Box key={data} gap="small" pad={{ right: "medium" }}>
                 <Video
-                  videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
-                  videoTitle="Official Music Video on YouTube"
+                  videoSrcURL={data.link}
+                  videoTitle={`class ${index + 1} ${data.sub} video`}
                 />
-                <Heading code={4}>Science</Heading>
+                <Heading code={4}>{data.sub}</Heading>
                 <Text code="sub-r" color="b2">
-                  Student studies the same concept in studynlearn package
+                  {data.description}
                 </Text>
               </Box>
             ))}
-          </Grid>
+          </MobileSwitcher>
         </Box>
       </Box>
     </Box>
