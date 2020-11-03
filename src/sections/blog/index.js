8fdc51a219ccdccccb54f23e65f5076c11434fd2
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-03 09:01:49 +0545

    Update the blog section to use the grid instead of slider

--- src/sections/blog/index.js
+++ src/sections/blog/index.js
@@ -1,50 +1,53 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Box, ResponsiveContext } from "grommet"
+import { Box, ResponsiveContext, Grid } from "grommet"
 import Heading from "../../components/heading"
 import Button from "../../components/button"
 import BlogCard from "./blogCard"
-import ComponentSlider from "../componentSlider"
 
 export default ({ title, url, data }) => {
   const mobile = React.useContext(ResponsiveContext) === "small"
 
   const blogPosts = []
-  data.allGoogleDocs.nodes.map(data =>
+  data.allCockpitBlog.edges.map(data =>
     blogPosts.push({
-      title: data.name,
-      minRead: data.childMarkdownRemark.timeToRead,
-      image: data.cover.image.childImageSharp.fluid,
-      link: data.path,
+      category: data.node.category.value,
+      title: data.node.title.value,
+      minRead: data.node.timeToRead.value,
+      image: data.node.coverImage.value.childImageSharp.fluid,
+      link: data.node.fields.path,
     })
   )
 
   return (
-    <Box>
-      <Box
-        pad={
-          mobile
-            ? { top: "90px", horizontal: "16px" }
-            : { top: "90px", horizontal: "131.5px" }
-        }
-      >
-        <Box direction="row">
-          <Heading code={2}>{title}</Heading>
-          <Box flex="grow">
-            {url ? (
-              <Box alignSelf="end">
-                <Link to={url}>
-                  <Button secondary label="view all" />
-                </Link>
-              </Box>
-            ) : null}
-          </Box>
+    <Box
+      pad={
+        mobile
+          ? { top: "90px", horizontal: "16px" }
+          : { top: "90px", horizontal: "131.5px" }
+      }
+    >
+      <Box direction="row">
+        <Heading code={2}>{title}</Heading>
+        <Box flex="grow">
+          {url ? (
+            <Box alignSelf="end">
+              <Link to={url}>
+                <Button secondary label="view all" />
+              </Link>
+            </Box>
+          ) : null}
         </Box>
       </Box>
-      <ComponentSlider margin={mobile ? { top: "16px" } : { top: "48px" }}>
+      <Grid
+        margin={mobile ? { top: "16px" } : { top: "48px" }}
+        columns={mobile ? "32%" : "100%"}
+        gap="small"
+      >
         {blogPosts.map((post, index) => (
           <BlogCard
             key={index}
+            category={post.category}
             mobile={mobile}
             title={post.title}
             image={post.image}
@@ -52,7 +55,7 @@ export default ({ title, url, data }) => {
             link={post.link}
           />
         ))}
-      </ComponentSlider>
+      </Grid>
     </Box>
   )
 }
