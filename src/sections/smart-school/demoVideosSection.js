44349b278b429cf8bcac684f5c1439520340e13d
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-03 17:03:56 +0545

    Add video section according to the design

--- src/sections/smart-school/demoVideosSection.js
+++ src/sections/smart-school/demoVideosSection.js
@@ -1,6 +1,9 @@
 import React from "react"
-import { Box, ResponsiveContext } from "grommet"
+import { Box, ResponsiveContext, Grid } from "grommet"
 import Heading from "../../components/heading"
+import Text from "../../components/text"
+import Button from "../../components/button"
+import Video from "./video"
 
 export default props => {
   const mobile = React.useContext(ResponsiveContext) === "small"
@@ -18,13 +21,42 @@ export default props => {
         gap="medium"
         margin={{ top: "32px" }}
         border={{ color: "w2", size: "2px" }}
+        direction="row"
+        height="536px"
       >
-        {/* Add Tabs and videos here */}
-        {/* <Tabs>
-          <Tab direction="column" title="Class 1">
-            <Box pad="medium">Add Tabs</Box>
-          </Tab>
-        </Tabs> */}
+        <Box
+          width="15%"
+          pad={{ left: "24px", top: "24px" }}
+          overflow="auto"
+          className="noScrollBar"
+          gap="small"
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(data => (
+            <Button
+              style={{ borderRadius: "8px", paddingRight: "35px" }}
+              ghost
+              alignSelf="start"
+              label={`Class ${data}`}
+              key={data}
+            />
+          ))}
+        </Box>
+        <Box flex="grow" pad={{ top: "24px", right: "24px" }}>
+          <Grid direction="row" columns="30%" gap="medium">
+            {[1, 2, 3, 4, 5].map(data => (
+              <Box key={data} gap="small">
+                <Video
+                  videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                  videoTitle="Official Music Video on YouTube"
+                />
+                <Heading code={4}>Science</Heading>
+                <Text code="sub-r" color="b2">
+                  Student studies the same concept in studynlearn package
+                </Text>
+              </Box>
+            ))}
+          </Grid>
+        </Box>
       </Box>
     </Box>
   )
