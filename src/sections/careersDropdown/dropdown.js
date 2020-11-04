3f3b6c77bee88d15c8c4a08275f79a4ade042fde
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-04 22:58:45 +0545

    Add job application form to the career dropdown section

--- src/sections/careersDropdown/dropdown.js
+++ src/sections/careersDropdown/dropdown.js
@@ -6,42 +6,54 @@ import Button from "../../components/button"
 import chevUp from "../../components/icons/chevron-up"
 import chevDown from "../../components/icons/chevron-down"
 
+import ApplyJob from "../../sections/applyJob"
+
 export default ({ info, active }) => {
   const [open, setOpen] = React.useState(active)
+  const [apply, setApply] = React.useState(false)
+
   const Icon = open ? chevUp : chevDown
 
   return (
-    <Box round="8px" border={{ size: "2px", color: "w2" }}>
-      <Box
-        direction="row"
-        onClick={() => setOpen(!open)}
-        background={open ? "p1-l" : "w1"}
-        pad="24px"
-        round={open ? { corner: "top", size: "8px" } : { size: "8px" }}
-        border={open ? { side: "bottom", size: "2px" } : ""}
-      >
-        <Heading code={4}>{info.position.value}</Heading>
-        <Box align="end" flex="grow">
-          <Icon color="b1" alignSelf="end" />
+    <>
+      <Box round="8px" border={{ size: "2px", color: "w2" }}>
+        <Box
+          direction="row"
+          onClick={() => setOpen(!open)}
+          background={open ? "p1-l" : "w1"}
+          pad="24px"
+          round={open ? { corner: "top", size: "8px" } : { size: "8px" }}
+          border={open ? { side: "bottom", size: "2px" } : ""}
+        >
+          <Heading code={4}>{info.position.value}</Heading>
+          <Box align="end" flex="grow">
+            <Icon color="b1" alignSelf="end" />
+          </Box>
         </Box>
+        <Collapsible open={open}>
+          <Box pad="24px" gap="small">
+            <Text code="sub-m" size="small">
+              Experience: {info.experience.value}
+            </Text>
+            <Text code="sub-m" size="small">
+              Requirement: {info.requirement.value}
+            </Text>
+            <Text code="sub-m" size="small">
+              Responsibilities:
+            </Text>
+            <Text code="sub-r" size="small" color="b2">
+              {info.responsibilites.value}
+            </Text>
+            <Button
+              secondary
+              label="Apply"
+              alignSelf="start"
+              onClick={() => setApply(true)}
+            />
+          </Box>
+        </Collapsible>
       </Box>
-      <Collapsible open={open}>
-        <Box pad="24px" gap="small">
-          <Text code="sub-m" size="small">
-            Experience: {info.experience.value}
-          </Text>
-          <Text code="sub-m" size="small">
-            Requirement: {info.requirement.value}
-          </Text>
-          <Text code="sub-m" size="small">
-            Responsibilities:
-          </Text>
-          <Text code="sub-r" size="small" color="b2">
-            {info.responsibilites.value}
-          </Text>
-          <Button secondary label="Apply" alignSelf="start" />
-        </Box>
-      </Collapsible>
-    </Box>
+      {apply && <ApplyJob setApply={setApply} />}
+    </>
   )
 }
