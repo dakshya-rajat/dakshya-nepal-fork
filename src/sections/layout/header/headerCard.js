d21b27b065fa5b689f804b14b47809e6c5636b59
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-05 02:30:08 +0545

    Add mobile menu for product list

--- src/sections/layout/header/headerCard.js
+++ src/sections/layout/header/headerCard.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Box, Image } from "grommet"
+import { Box, Image, ResponsiveContext } from "grommet"
 import Heading from "../../../components/heading"
+import Text from "../../../components/text"
 import ArrowRight from "../../../components/icons/arrow-right"
 
 export default ({ product }) => {
@@ -10,27 +11,40 @@ export default ({ product }) => {
       to={product.link}
       style={{ textDecoration: "none", color: "inherit" }}
     >
-      <Box
-        height="xsmall"
-        direction="row"
-        round="4px"
-        gap="small"
-        border={{ size: "2px" }}
-      >
-        <Box
-          background="p1-l"
-          width="25%"
-          round={{ corner: "left", size: "4px" }}
-        >
-          <Image src={product.image} fit="contain" />
-        </Box>
-        <Box flex="grow" alignSelf="center" direction="row">
-          <Heading code={4}>{product.name}</Heading>
-          <Box flex="grow" align="end" pad={{ right: "small" }}>
-            <ArrowRight />
-          </Box>
-        </Box>
-      </Box>
+      <ResponsiveContext.Consumer>
+        {size =>
+          size === "small" ? (
+            <Box
+              border={{ side: "bottom", color: "b1" }}
+              pad={{ bottom: "small" }}
+            >
+              <Text code="body">{product.name}</Text>
+            </Box>
+          ) : (
+            <Box
+              height="xsmall"
+              direction="row"
+              round="4px"
+              gap="small"
+              border={{ size: "2px" }}
+            >
+              <Box
+                background="p1-l"
+                width="25%"
+                round={{ corner: "left", size: "4px" }}
+              >
+                <Image src={product.image} fit="contain" />
+              </Box>
+              <Box flex="grow" alignSelf="center" direction="row">
+                <Heading code={4}>{product.name}</Heading>
+                <Box flex="grow" align="end" pad={{ right: "small" }}>
+                  <ArrowRight />
+                </Box>
+              </Box>
+            </Box>
+          )
+        }
+      </ResponsiveContext.Consumer>
     </Link>
   )
 }
