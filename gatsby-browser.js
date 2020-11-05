2fbac0855bb506fcf84a72ee98383e1bf06064c4
dakshya-rajat <70906749+dakshya-rajat@users.noreply.github.com>
2020-11-05 14:10:34 +0545

    add favicon and tawk.to chat widget

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,6 +12,17 @@ import "./src/styles/reset.css"
 
 // Wraps every page in a component
 export const wrapPageElement = ({ element, props }) => {
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date()
+  ;(function () {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0]
+    s1.async = true
+    s1.src = "https://embed.tawk.to/5f9a779f7f0a8e57c2d81cb4/default"
+    s1.charset = "UTF-8"
+    s1.setAttribute("crossorigin", "*")
+    s0.parentNode.insertBefore(s1, s0)
+  })()
   return (
     <Grommet {...props} theme={dakshyaTheme}>
       {element}
