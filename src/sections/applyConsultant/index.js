d404a6ad6a726b43351707b9449996ae3cfdbd48
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-06 13:04:47 +0545

    Fix design issues with the form

--- src/sections/applyConsultant/index.js
+++ src/sections/applyConsultant/index.js
@@ -7,6 +7,7 @@ import Exit from "../../components/icons/exit"
 import { Field, Formik } from "formik"
 import Notification from "../notification"
 import Text from "../../components/text"
+import { RemoveScroll } from "react-remove-scroll"
 
 export default props => {
   const mobile = React.useContext(ResponsiveContext) === "small"
@@ -17,227 +18,235 @@ export default props => {
   const [error, setError] = React.useState(false)
 
   return (
-    <Layer
-      animation="slide"
-      position="center"
-      onClickOutside={() => props.setApply(false)}
-      onEsc={() => props.setApply(false)}
-      modal={true}
-    >
-      <Box direction="row" elevation="d1" align="center">
-        <Box
-          pad="24px"
-          gap="medium"
-          width={mobile ? "100%" : "50%"}
-          height="560px"
-          overflow="auto"
-        >
-          <Box direction="row">
-            <Image alignSelf="start" src="/images/logo-horizontal.svg" />
-            {mobile ? (
-              <Box flex="grow" alignContent="end">
-                <Box onClick={() => props.setApply(false)} alignSelf="end">
-                  <Exit />
-                </Box>
-              </Box>
-            ) : null}
-          </Box>
-          <Heading margin={{ top: "medium" }} code={3}>
-            Apply for counselor
-          </Heading>
-          <Formik
-            initialValues={{
-              name: "",
-              gender: "",
-              email: "",
-              phone: "",
-              address: "",
-              qualification: "",
-              specialization: "",
-              training: "",
-              occupation: "",
-              whyAssociate: "",
-              position: "Consultant",
-            }}
-            onSubmit={async (values, { setSubmitting, resetForm }) => {
-              const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  "Cockpit-Token": process.env.GATSBY_API_KEY,
-                },
-                mode: "cors",
-                body: JSON.stringify({ form: values }),
-              })
-
-              if (response.ok) {
-                setSuccess(true)
-                setSubmitting(false)
-                resetForm()
-              } else setError(true)
-            }}
+    <RemoveScroll>
+      <Layer
+        onClickOutside={() => props.setApply(false)}
+        onEsc={() => props.setApply(false)}
+        modal={true}
+      >
+        <Box direction="row">
+          <Box
+            pad="24px"
+            gap="small"
+            width={mobile ? "100%" : "60%"}
+            height="560px"
+            overflow="auto"
           >
-            {({
-              values,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <FormField
-                  label="Your Name *"
-                  placeholder="Jane Doe"
-                  type="text"
-                  name="name"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.name}
-                  required={true}
-                />
-                <Box gap="xsmall">
-                  <Text size="small">Gender</Text>
-                  <Field name="gender" as="select">
-                    <option value="" disabled selected hidden>
-                      Select one
-                    </option>
-                    <option value="Academics">Male</option>
-                    <option value="Relationship">Female</option>
-                    <option value="Others">Others</option>
-                  </Field>
-                </Box>
-                <FormField
-                  label="Your Email*"
-                  placeholder="email@domain.com"
-                  type="text"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.email}
-                  required={true}
-                />
-                <FormField
-                  label="Contact Number"
-                  placeholder="9876564123"
-                  type="text"
-                  name="phone"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.phone}
-                  required={true}
-                />
-                <label
-                  style={{
-                    fontSize: "14px",
-                    lineHeight: "normal",
-                  }}
-                  htmlFor="address"
-                >
-                  Address
-                </label>
-                <TextArea
-                  placeholder="Input your address"
-                  size="small"
-                  name="address"
-                  style={{ border: "2px solid #EAEAEA" }}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.address}
-                />
-                <FormField
-                  label="Education Qualification"
-                  placeholder="Undergraduate"
-                  type="text"
-                  name="qualification"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.qualification}
-                  required={true}
-                />
-                <FormField
-                  label="Any Specialization In Counseling"
-                  placeholder="Training"
-                  type="text"
-                  name="specialization"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.specialization}
-                  required={true}
-                />
-                <FormField
-                  label="Counseling Or Related Subjects Training Or Courses"
-                  placeholder="Your Training"
-                  type="text"
-                  name="training"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.training}
-                  required={true}
-                />
-                <FormField
-                  label="Current Occupation"
-                  placeholder="Your Occupation"
-                  type="text"
-                  name="occupation"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.occupation}
-                  required={true}
-                />
-                <label
-                  style={{
-                    fontSize: "14px",
-                    lineHeight: "normal",
-                  }}
-                  htmlFor="dakshya"
-                >
-                  Why do you want to associate with Dakshya Nepal?
-                </label>
-                <TextArea
-                  placeholder="Input your answer"
-                  size="small"
-                  name="whyAssociate"
-                  style={{ border: "2px solid #EAEAEA" }}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.whyAssociate}
-                />
-                <input type="hidden" name="position" value={values.position} />
-                <Box pad={{ top: "small" }}>
-                  <Button
-                    type="submit"
-                    alignSelf="start"
-                    primary
-                    label="Submit"
-                    disabled={isSubmitting}
+            <Box direction="row" margin={{ bottom: "large" }}>
+              <Heading code={4}>Apply for counselor</Heading>
+              <ResponsiveContext.Consumer>
+                {size =>
+                  size === "small" ? (
+                    <Box alignContent="end" flex="grow">
+                      <Box
+                        onClick={() => props.setApply(false)}
+                        alignSelf="end"
+                      >
+                        <Exit />
+                      </Box>
+                    </Box>
+                  ) : null
+                }
+              </ResponsiveContext.Consumer>
+            </Box>
+            <Formik
+              initialValues={{
+                name: "",
+                gender: "",
+                email: "",
+                phone: "",
+                address: "",
+                qualification: "",
+                specialization: "",
+                training: "",
+                occupation: "",
+                whyAssociate: "",
+                position: "Consultant",
+              }}
+              onSubmit={async (values, { setSubmitting, resetForm }) => {
+                const response = await fetch(url, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "Cockpit-Token": process.env.GATSBY_API_KEY,
+                  },
+                  mode: "cors",
+                  body: JSON.stringify({ form: values }),
+                })
+
+                if (response.ok) {
+                  setSuccess(true)
+                  setSubmitting(false)
+                  resetForm()
+                } else setError(true)
+              }}
+            >
+              {({
+                values,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <FormField
+                    label="Your Name *"
+                    placeholder="Jane Doe"
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.name}
+                    required={true}
                   />
-                </Box>
-              </form>
+                  <Box gap="xsmall">
+                    <Text size="small">Gender</Text>
+                    <Field name="gender" as="select">
+                      <option value="" disabled selected hidden>
+                        Select one
+                      </option>
+                      <option value="Academics">Male</option>
+                      <option value="Relationship">Female</option>
+                      <option value="Others">Others</option>
+                    </Field>
+                  </Box>
+                  <FormField
+                    label="Your Email*"
+                    placeholder="email@domain.com"
+                    type="text"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.email}
+                    required={true}
+                  />
+                  <FormField
+                    label="Contact Number"
+                    placeholder="9876564123"
+                    type="text"
+                    name="phone"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.phone}
+                    required={true}
+                  />
+                  <label
+                    style={{
+                      fontSize: "14px",
+                      lineHeight: "normal",
+                    }}
+                    htmlFor="address"
+                  >
+                    Address
+                  </label>
+                  <TextArea
+                    placeholder="Input your address"
+                    size="small"
+                    name="address"
+                    style={{ border: "2px solid #EAEAEA" }}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.address}
+                  />
+                  <FormField
+                    label="Education Qualification"
+                    placeholder="Undergraduate"
+                    type="text"
+                    name="qualification"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.qualification}
+                    required={true}
+                  />
+                  <FormField
+                    label="Any Specialization In Counseling"
+                    placeholder="Training"
+                    type="text"
+                    name="specialization"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.specialization}
+                    required={true}
+                  />
+                  <FormField
+                    label="Counseling Or Related Subjects Training Or Courses"
+                    placeholder="Your Training"
+                    type="text"
+                    name="training"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.training}
+                    required={true}
+                  />
+                  <FormField
+                    label="Current Occupation"
+                    placeholder="Your Occupation"
+                    type="text"
+                    name="occupation"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.occupation}
+                    required={true}
+                  />
+                  <label
+                    style={{
+                      fontSize: "14px",
+                      lineHeight: "normal",
+                    }}
+                    htmlFor="dakshya"
+                  >
+                    Why do you want to associate with Dakshya Nepal?
+                  </label>
+                  <TextArea
+                    placeholder="Input your answer"
+                    size="small"
+                    name="whyAssociate"
+                    style={{ border: "2px solid #EAEAEA" }}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.whyAssociate}
+                  />
+                  <input
+                    type="hidden"
+                    name="position"
+                    value={values.position}
+                  />
+                  <Box pad={{ top: "small" }}>
+                    <Button
+                      type="submit"
+                      alignSelf="start"
+                      primary
+                      label="Submit"
+                      disabled={isSubmitting}
+                    />
+                  </Box>
+                </form>
+              )}
+            </Formik>
+            {success && <Notification message="Message Sent Successfully" />}
+            {error && (
+              <Notification
+                message="Could not send message. Please try again!"
+                error
+              />
             )}
-          </Formik>
-          {success && <Notification message="Message Sent Successfully" />}
-          {error && (
-            <Notification
-              message="Could not send message. Please try again!"
-              error
-            />
+          </Box>
+          {mobile ? null : (
+            <Box background="p1-l" width="50%">
+              <Box
+                style={{ position: "absolute", top: "24px", right: "24px" }}
+                onClick={() => props.setApply(false)}
+              >
+                <Exit />
+              </Box>
+              <Box height="570px">
+                <Image src="/illustrations/guidance form.svg" fit="cover" />
+              </Box>
+            </Box>
           )}
         </Box>
-        {mobile ? null : (
-          <Box background="p1-l" width="50%">
-            <Box
-              style={{ position: "absolute", top: "24px", right: "24px" }}
-              onClick={() => props.setApply(false)}
-            >
-              <Exit />
-            </Box>
-            <Box height="570px">
-              <Image src="/illustrations/guidance form.svg" fit="cover" />
-            </Box>
-          </Box>
-        )}
-      </Box>
-    </Layer>
+      </Layer>
+    </RemoveScroll>
   )
 }
--- src/sections/applyJob/index.js
+++ src/sections/applyJob/index.js
@@ -6,6 +6,7 @@ import Button from "../../components/button"
 import Exit from "../../components/icons/exit"
 import { Formik } from "formik"
 import Notification from "../notification"
+import { RemoveScroll } from "react-remove-scroll"
 
 export default props => {
   const mobile = React.useContext(ResponsiveContext) === "small"
@@ -16,152 +17,165 @@ export default props => {
   const [error, setError] = React.useState(false)
 
   return (
-    <Layer
-      onClickOutside={() => props.setApply(false)}
-      onEsc={() => props.setApply(false)}
-      modal={true}
-    >
-      <Box direction="row">
-        <Box pad="24px" gap="small" width={mobile ? "100%" : "50%"}>
-          <Box direction="row">
-            {mobile ? (
-              <Box flex="grow" alignContent="end">
-                <Box onClick={() => props.setApply(false)} alignSelf="start">
-                  <Exit />
-                </Box>
-              </Box>
-            ) : null}
-          </Box>
-          <Heading code={4}>Apply for {props.position}</Heading>
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              phone: "",
-              message: "",
-              driveUrl: "",
-              position: props.position,
-            }}
-            onSubmit={async (values, { setSubmitting, resetForm }) => {
-              const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  "Cockpit-Token": process.env.GATSBY_API_KEY,
-                },
-                mode: "cors",
-                body: JSON.stringify({ form: values }),
-              })
+    <RemoveScroll>
+      <Layer
+        onClickOutside={() => props.setApply(false)}
+        onEsc={() => props.setApply(false)}
+        modal={true}
+      >
+        <Box direction="row">
+          <Box pad="24px" gap="small" width={mobile ? "100%" : "50%"}>
+            <Box direction="row" margin={{ bottom: "large" }}>
+              <Heading code={4}>Apply for {props.position}</Heading>
+              <ResponsiveContext.Consumer>
+                {size =>
+                  size === "small" ? (
+                    <Box alignContent="end" flex="grow">
+                      <Box
+                        onClick={() => props.setApply(false)}
+                        alignSelf="end"
+                      >
+                        <Exit />
+                      </Box>
+                    </Box>
+                  ) : null
+                }
+              </ResponsiveContext.Consumer>
+            </Box>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                phone: "",
+                message: "",
+                driveUrl: "",
+                position: props.position,
+              }}
+              onSubmit={async (values, { setSubmitting, resetForm }) => {
+                const response = await fetch(url, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "Cockpit-Token": process.env.GATSBY_API_KEY,
+                  },
+                  mode: "cors",
+                  body: JSON.stringify({ form: values }),
+                })
 
-              if (response.ok) {
-                setSuccess(true)
-                setSubmitting(false)
-                resetForm()
-              } else setError(true)
-            }}
-          >
-            {({
-              values,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <FormField
-                  label="Your Name *"
-                  placeholder="Jane Doe"
-                  type="text"
-                  name="name"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.name}
-                  required={true}
-                />
-                <FormField
-                  label="Your Email*"
-                  placeholder="email@domain.com"
-                  type="text"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.email}
-                  required={true}
-                />
-                <FormField
-                  label="Your Phone*"
-                  placeholder="9854756256"
-                  type="text"
-                  name="phone"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.phone}
-                  required={true}
-                />
-                <label
-                  style={{
-                    fontSize: "14px",
-                    lineHeight: "normal",
-                  }}
-                  htmlFor="message"
-                >
-                  Messages (If any)
-                </label>
-                <TextArea
-                  placeholder="Input your message"
-                  size="small"
-                  name="message"
-                  style={{ border: "2px solid #EAEAEA" }}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.message}
-                />
-                <FormField
-                  label="Your Resume Link *"
-                  placeholder="Google Drive Link"
-                  type="text"
-                  name="driveUrl"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  values={values.driveUrl}
-                  required={true}
-                />
-                <input type="hidden" name="position" value={values.position} />
-                <Box pad={{ top: "xsmall" }}>
-                  <Button
-                    type="submit"
-                    alignSelf="start"
-                    primary
-                    label="Submit"
-                    disabled={isSubmitting}
+                if (response.ok) {
+                  setSuccess(true)
+                  setSubmitting(false)
+                  resetForm()
+                } else setError(true)
+              }}
+            >
+              {({
+                values,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <FormField
+                    label="Your Name *"
+                    placeholder="Jane Doe"
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.name}
+                    required={true}
                   />
-                </Box>
-              </form>
+                  <FormField
+                    label="Your Email*"
+                    placeholder="email@domain.com"
+                    type="text"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.email}
+                    required={true}
+                  />
+                  <FormField
+                    label="Your Phone*"
+                    placeholder="9854756256"
+                    type="text"
+                    name="phone"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.phone}
+                    required={true}
+                  />
+                  <label
+                    style={{
+                      fontSize: "14px",
+                      lineHeight: "normal",
+                    }}
+                    htmlFor="message"
+                  >
+                    Messages (If any)
+                  </label>
+                  <TextArea
+                    placeholder="Input your message"
+                    size="small"
+                    name="message"
+                    style={{ border: "2px solid #EAEAEA" }}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.message}
+                  />
+                  <FormField
+                    label="Your Resume Link *"
+                    placeholder="Google Drive Link"
+                    type="text"
+                    name="driveUrl"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    values={values.driveUrl}
+                    required={true}
+                  />
+                  <input
+                    type="hidden"
+                    name="position"
+                    value={values.position}
+                  />
+                  <Box pad={{ top: "xsmall" }}>
+                    <Button
+                      type="submit"
+                      alignSelf="start"
+                      primary
+                      label="Submit"
+                      disabled={isSubmitting}
+                    />
+                  </Box>
+                </form>
+              )}
+            </Formik>
+            {success && <Notification message="Message Sent Successfully" />}
+            {error && (
+              <Notification
+                message="Could not send message. Please try again!"
+                error
+              />
             )}
-          </Formik>
-          {success && <Notification message="Message Sent Successfully" />}
-          {error && (
-            <Notification
-              message="Could not send message. Please try again!"
-              error
-            />
+          </Box>
+          {mobile ? null : (
+            <Box background="p1-l" width="50%">
+              <Box
+                style={{ position: "absolute", top: "24px", right: "24px" }}
+                onClick={() => props.setApply(false)}
+              >
+                <Exit />
+              </Box>
+              <Box height="570px">
+                <Image src="/illustrations/guidance form.svg" fit="cover" />
+              </Box>
+            </Box>
           )}
         </Box>
-        {mobile ? null : (
-          <Box background="p1-l" width="50%">
-            <Box
-              style={{ position: "absolute", top: "24px", right: "24px" }}
-              onClick={() => props.setApply(false)}
-            >
-              <Exit />
-            </Box>
-            <Box height="570px">
-              <Image src="/illustrations/guidance form.svg" fit="cover" />
-            </Box>
-          </Box>
-        )}
-      </Box>
-    </Layer>
+      </Layer>
+    </RemoveScroll>
   )
 }
