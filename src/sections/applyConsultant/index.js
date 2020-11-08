de66a61aac8423e59018af12c7baf23df03778b5
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-08 06:57:01 +0545

    Add same origin cors value for security reasons

--- src/sections/applyConsultant/index.js
+++ src/sections/applyConsultant/index.js
@@ -70,7 +70,7 @@ export default props => {
                     "Content-Type": "application/json",
                     "Cockpit-Token": process.env.GATSBY_API_KEY,
                   },
-                  mode: "cors",
+                  mode: "no-cors",
                   body: JSON.stringify({ form: values }),
                 })
 
