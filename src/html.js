59b98c34f43ad5202975de9fece5bf9993e9e0c1
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-08 12:00:29 +0545

    Remove mapbox css

--- src/html.js
+++ src/html.js
@@ -11,10 +11,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
-          rel="stylesheet"
-        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
