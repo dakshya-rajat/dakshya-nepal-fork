ce7d1241ca65e314bc094483568e667d2372f36f
Yogendra Rawal <yogendra.rawal@dakshyanepal.com>
2020-11-29 13:55:28 +0545

    Update the blog section to fetch the latest blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,45 +3,22 @@ import React from "react"
 import Layout from "../sections/layout"
 import SEO from "../components/seo"
 import Blog from "../sections/blog"
-import { graphql } from "gatsby"
 
-export default ({ data }) => {
+export default () => {
+  const [blogData, setBlogData] = React.useState([])
+
+  React.useEffect(() => {
+    // get data from api
+    fetch(`${process.env.GATSBY_API_URL}/api/collections/get/blog`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setBlogData(resultData.entries)
+      }) // set data for the blog
+  }, [])
   return (
     <Layout active="blogs">
       <SEO title="Blogs" />
-      <Blog title="All Blogs" data={data} />
+      <Blog title="All Blogs" data={blogData} />
     </Layout>
   )
 }
-
-export const query = graphql`
-  {
-    allCockpitBlog(filter: { lang: { eq: "en" } }) {
-      edges {
-        node {
-          fields {
-            path
-          }
-          title {
-            value
-          }
-          category {
-            value
-          }
-          timeToRead {
-            value
-          }
-          coverImage {
-            value {
-              childImageSharp {
-                fluid {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
